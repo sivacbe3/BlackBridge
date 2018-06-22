2f4b2da158ffb9c9baa9429b61d446303bfb92d6
sivakumar <sivakumar@DESKTOP-PM35DO9>
2018-06-22 13:36:42 +0530

    Massive Hit

---
+++ BlackBridge/src/main/java/com/Google/maven/BlackBridge/App.java
@@ -0,0 +1,13 @@
+package com.Google.maven.BlackBridge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
